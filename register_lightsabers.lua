0fa8d9ea404c4874b36774f4409cf37b5605709d
ElCeejo <40281901+ElCeejoYT@users.noreply.github.com>
2020-04-11 20:03:49 -0700

    Undo previous commits

--- register_lightsabers.lua
+++ register_lightsabers.lua
@@ -233,8 +233,9 @@ function adv_lightsabers:register_lightsaber(type,color)
 
         minetest.register_craftitem("adv_lightsabers:lightsaber_single_"..color.."_on", {
             description = "Lightsaber",
-            inventory_image = "adv_lightsabers_blade_single_"..color..".png^adv_lightsabers_hilt_single.png",
-            wield_scale = {x = 4,y = 4,z = 1},
+            inventory_image = "adv_lightsabers_hilt_single_inv.png",
+            wield_image = "adv_lightsabers_blade_single_"..color..".png^adv_lightsabers_hilt_single.png",
+            wield_scale = {x = 2,y = 2,z = 1},
             stack_max = 1,
             on_use = function(itemstack,player,pointed_thing)
                 local swing = "adv_lightsabers_swing"
@@ -283,8 +284,9 @@ function adv_lightsabers:register_lightsaber(type,color)
 
         minetest.register_craftitem("adv_lightsabers:lightsaber_cross_"..color.."_on", {
             description = "Crossguarded Lightsaber",
-            inventory_image = "adv_lightsabers_blade_cross_"..color..".png^adv_lightsabers_hilt_cross.png",
-            wield_scale = {x = 4,y = 4,z = 1},
+            inventory_image = "adv_lightsabers_hilt_cross_inv.png",
+            wield_image = "adv_lightsabers_blade_cross_"..color..".png^adv_lightsabers_hilt_cross.png",
+            wield_scale = {x = 2,y = 2,z = 1},
             stack_max = 1,
             on_use = function(itemstack,player,pointed_thing)
                 local swing = "adv_lightsabers_swing_cross"
@@ -333,8 +335,9 @@ function adv_lightsabers:register_lightsaber(type,color)
 
         minetest.register_craftitem("adv_lightsabers:lightsaber_double_"..color.."_on", {
             description = "Lightsaber",
-            inventory_image = "adv_lightsabers_hilt_double.png^adv_lightsabers_blade_double_"..color..".png",
-            wield_scale = {x = 6,y = 6,z = 1},
+            inventory_image = "adv_lightsabers_hilt_double_inv.png",
+            wield_image = "adv_lightsabers_hilt_double.png^adv_lightsabers_blade_double_"..color..".png",
+            wield_scale = {x = 4,y = 4,z = 1},
             stack_max = 1,
             on_use = function(itemstack,player,pointed_thing)
                 local swing = "adv_lightsabers_swing"
